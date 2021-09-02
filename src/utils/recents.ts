8c2eec6f7476394bb202d767f41380aac4c9df95
Bruno Saibert <brunosaibert@gmail.com>
2021-09-01 21:58:31 -0300

    feat: data recents

---
+++ src/utils/recents.ts
@@ -0,0 +1,32 @@
+export const dataRecents = [
+  {
+    id: '1',
+    title: 'Músicas Curtidas',
+    imageUrl: require('../assets/images/liked.jpg'),
+  },
+  {
+    id: '2',
+    title: 'Queens Of The Stone Age',
+    imageUrl: require('../assets/images/qotsa.jpg'),
+  },
+  {
+    id: '3',
+    title: 'Cage the Elephant',
+    imageUrl: require('../assets/images/cage_the_elephant.jpg'),
+  },
+  {
+    id: '4',
+    title: 'Portugal. The Man',
+    imageUrl: require('../assets/images/portugal_the_man.jpg'),
+  },
+  {
+    id: '5',
+    title: 'Weezer',
+    imageUrl: require('../assets/images/weezer.jpg'),
+  },
+  {
+    id: '6',
+    title: 'Tame Impala',
+    imageUrl: require('../assets/images/tame_impala.jpg'),
+  },
+];
